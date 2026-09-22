3e080015d6f3627684c6b06e75e6e356da4a99ba
ebradford <emma_bradford@milton.edu>
2026-09-22 19:24:35 -0400

    getters and setters

---
+++ src/Gorilla.java
@@ -0,0 +1,26 @@
+public class Gorilla {
+
+    private int bananasConsumed;
+    private int wieght;
+
+
+    public Gorilla(){
+        bananasConsumed = 2060;
+        wieght = 213;
+    }
+
+    //getter
+    public int getBananasConsumed(){
+        return bananasConsumed;
+    }
+
+    //setter
+    public void setBananasConsumed(int bananasConsumed){
+        this.bananasConsumed = bananasConsumed;
+    }
+
+    //todo: to make the getter and setter for wieght
+    //todo: use the getters and setters in the zoo
+
+
+}
--- src/Zoo.java
+++ src/Zoo.java
@@ -6,6 +6,11 @@ public class Zoo {//
     public Zoo(){
 Zebra a = new Zebra();
 a.makeSound();
+Gorilla g = new Gorilla();
+        System.out.println(g.getBananasConsumed() + " bananas!");
+
+        g.setBananasConsumed(4090);
+        System.out.println(g.getBananasConsumed() + " bananas!");
     }
 
     public void method1(){
